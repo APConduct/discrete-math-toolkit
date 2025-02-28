11747838a16d0ddc79d822130dab57913dfc98fa
Perry <aidanpj18@gmail.com>
2025-02-28 00:40:57 -0500

    init lua files

---
+++ src/init.lua
@@ -0,0 +1,12 @@
+local discrete_math = {
+    Set = require "src.set",
+    NumberTheory = require "src.number_theory",
+    Graph = require "src.graph",
+    graph_theory = require "src.graph_theory",
+    Logic = require "src.logic",
+    relations = require "src.relations",
+    Sequence = require "src.sequence",
+    combinatorics = require "src.combinatorics"
+}
+
+return discrete_math
